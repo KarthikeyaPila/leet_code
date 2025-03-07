b6358b81274f88ced12ff6b496674c5606fb3708
Josh Kumar Polamarasetti <josh.kumar@ballys.com>
2025-03-07 22:39:22 +0000

    feat(lc-1499): add working solution for findMaxValofEqn

--- Hard/1499/solution.c
+++ Hard/1499/solution.c
@@ -1,26 +1,30 @@
-#include <stdio.h> 
-#include "solution.h"
+int mod(int x) {
+  if(x < 0){
+    return x*-1;
+  }
 
-int findMaxValueOfEquation(int** points, int pointsSize, int* pointsColSize, int k) {
-    int max = 0; 
-    int abs_max = 0;
-// max = yi + yj + |xi - xj|   
+  return x;
+}
+
+// max = yi + yj + |xi - xj|
 // |xi - xj| <= k
+int findMaxValueOfEquation(int **points, int pointsSize, int *pointsColSize,
+                           int k) {
+  int abs_max = 0;
 
-    for(int i=0; i<pointsSize; i++){
-        for(int j=i; j<pointsSize; j++){
-            int mod = points[i][0] + points[j][0];
-            mod = mod < 0 ? mod*-1 : mod;
+  for (int i = 0; i < pointsSize-1; i++) {
+    int total = 0;
+    int val = points[i][0] - points[i+1][0];
+    int val_mod = mod(val);
 
-            if(mod <= k){
-                max = points[i][1] + points[j][1] + mod;
-            }
-
-            if(max > abs_max){
-                abs_max = max; 
-            }
-        }
+    if (val_mod <= k) {
+      total = points[i][1] + points[i+1][1] + val_mod;
     }
 
-    return abs_max;
+    if (total > abs_max) {
+      abs_max = total;
+    }
+  }
+
+  return abs_max;
 }
