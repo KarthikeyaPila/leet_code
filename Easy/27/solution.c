c223c8b73fbadad18e617b250eeefe232d6988b6
karthikeya <karthikeyapila3@gmail.com>
2025-04-04 00:38:04 +0530

    solve 27 leetcode: Remove Element

---
+++ Easy/27/solution.c
@@ -0,0 +1,27 @@
+int removeElement(int* nums, int numsSize, int val) {
+
+    if(numsSize == 0){
+        return;
+    }
+
+    //[0,1,2,2,3,0,4,2]     val = 2
+    // 0 1 3 0 4 ----->ans
+
+    int ans[numsSize];
+    int k=0, count=0;
+    for(int i=0; i<numsSize; i++){
+        if(nums[i] != val){
+            ans[k] = nums[i];
+            k++;
+        }
+        else if(nums[i] == val){
+            count++;
+        }
+    }
+    
+    for(int i=0; i<k; i++){
+        nums[i] = ans[i];
+    }
+
+    return numsSize - count;
+}
